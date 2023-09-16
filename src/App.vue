529b0c90f94b9d7578476d294fb8a736eba0d50f
po-heng-home <>
2023-09-16 12:30:40 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="penny logo" height="500" src="https://lh3.googleusercontent.com/pw/AIL4fc-NVTBSSyuRsdoU64NXQ_cMwZhoUtErO6OZTKwYk3OGhAS5Uc59momHDAtcq_CKYGQDUrJ0ETYdKIog5tCHsWSIC6WpuBYZOJiJgWWh2TUihmyoyPvv32bylaM4-636P1ZIxeKSXHKEB49uFl87XRQsPZbndYuWPWtMR6kj7gvhvBoFwTkddlvNxSf8k5AFK6e-ZBYvK1Ya_PHhehNpdpJfzsp-dz8FjgpfcBAYesZfw8tURQ1NBfbfojHI5tc7w_dynrhNttgIjUhBKc1wji2XntWq2bwj7MOgQO3gFdHpoUslB84CDBoRZGfpBEfE9vGJHyUJd98M8fwPA9KX3G_5KjhMRT3OA2OFfBz6mfi_UU6Kd4ye4hGJKXEHIKOiq3w8FJvg8kY2VrVvkwxpiXgJCos-pfDKQIGSlfR67p-RBdWtBnNWwcRAAsD0czMxOkUKDfCnBNp_39CF9XO7sYCXZR4E1q432ugDUA1T6n9mp3cZWi_ucmJZKIiPqsxuFxdmtMEGGoPH7Xg7WrL8w8p3GG7MBcxJP_py6xBGQIWV0JRlwKN9CUmSYxRiEZq7lpm6eQZsbmfacy7rvPSYiVMYPW8-wrH1fQLLbXxXysQYkdio_l39NT4zmL_luB269QENWNqreLJk0Ok5WGc9hOUGX--KgGpGsBzb5mhrxRlbMYaelQTax_N5QZm6ccFcca7SP1Mw-Ud_tvmy-rI2MvyO0bkICgnK25fgYKRlarsL9KrpaA_adikSR6Ozpq2EoAlWVjaMOykqj5qFIy5xaaXRrvfUNE5n6-Zi45LphKMhIy9Y2V056ymdPOZJR5gUhhaCePB6XgW4_Poj4Kx4S3wcFlwvrR9QMDNNQt7lNUaY4OqVYTqrT4UX6JtCcpXMq5Pxss0jX8-VynlDnXWfjIU=w600-h400-s-no?authuser=0">
+  <HelloWorld msg="房屋網"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+     <h3>房屋照片</h3>  
+      <br>
+       <a href="https://photos.google.com/share/AF1QipNhcMeC_KwHR5Y0y0dwjF8ob5s1ch-1ax-Hg67hzreUtAhG33Db0VbOSTCHDpeGNg?key=cWgzLUtwZFdIUV9ZcTdSa3lWSWVhNDI0NGVRaE5R" target="_blank" rel="noopener">點我->>>看房屋</a>.
+   <br>
+  <br>
+ <br>
+  <br>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
