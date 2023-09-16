d446293959bd270cae3798d9583c6e0bc17b9824
po-heng-home <>
2023-09-16 15:23:19 +0800

    切版

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
   <img alt="house logo" height="500" src="https://lh3.googleusercontent.com/pw/AIL4fc-NVTBSSyuRsdoU64NXQ_cMwZhoUtErO6OZTKwYk3OGhAS5Uc59momHDAtcq_CKYGQDUrJ0ETYdKIog5tCHsWSIC6WpuBYZOJiJgWWh2TUihmyoyPvv32bylaM4-636P1ZIxeKSXHKEB49uFl87XRQsPZbndYuWPWtMR6kj7gvhvBoFwTkddlvNxSf8k5AFK6e-ZBYvK1Ya_PHhehNpdpJfzsp-dz8FjgpfcBAYesZfw8tURQ1NBfbfojHI5tc7w_dynrhNttgIjUhBKc1wji2XntWq2bwj7MOgQO3gFdHpoUslB84CDBoRZGfpBEfE9vGJHyUJd98M8fwPA9KX3G_5KjhMRT3OA2OFfBz6mfi_UU6Kd4ye4hGJKXEHIKOiq3w8FJvg8kY2VrVvkwxpiXgJCos-pfDKQIGSlfR67p-RBdWtBnNWwcRAAsD0czMxOkUKDfCnBNp_39CF9XO7sYCXZR4E1q432ugDUA1T6n9mp3cZWi_ucmJZKIiPqsxuFxdmtMEGGoPH7Xg7WrL8w8p3GG7MBcxJP_py6xBGQIWV0JRlwKN9CUmSYxRiEZq7lpm6eQZsbmfacy7rvPSYiVMYPW8-wrH1fQLLbXxXysQYkdio_l39NT4zmL_luB269QENWNqreLJk0Ok5WGc9hOUGX--KgGpGsBzb5mhrxRlbMYaelQTax_N5QZm6ccFcca7SP1Mw-Ud_tvmy-rI2MvyO0bkICgnK25fgYKRlarsL9KrpaA_adikSR6Ozpq2EoAlWVjaMOykqj5qFIy5xaaXRrvfUNE5n6-Zi45LphKMhIy9Y2V056ymdPOZJR5gUhhaCePB6XgW4_Poj4Kx4S3wcFlwvrR9QMDNNQt7lNUaY4OqVYTqrT4UX6JtCcpXMq5Pxss0jX8-VynlDnXWfjIU=w600-h400-s-no?authuser=0">
   <HelloWorld msg="房屋網"/>
+  <div class="home">
+    <Navbar></Navbar>
+    <Header></Header>
+    <Banner></Banner>
+    <Footer></Footer>
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from '@/components/HomeHeader.vue'; 
+import Banner from '@/components/HomeBanner.vue'; 
+import Footer from '@/components/HomeFooter.vue'; 
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, Header, Banner, Footer
   }
 }
 </script>
---
+++ src/components/HomeFooter.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="footer">
+        <div class="container h-100">
+            <div class="row p-5 h-100 justify-content-center align-items-center">
+            <!--footer畫面左區-->
+            <div class="col-sm-12 col-md-7">
+                <h3>衛浴設備</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <button @click="gostores" class="btn custom-btn btn-outline-primary mr-4">
+                  Stores Information
+                </button>
+            </div>
+            <!--footer畫面右區-->
+            <div class="col-sm-12 col-md-4 m-3">
+                <img src="/img/layout/webpic.jpg" alt="" width="300">
+            </div>
+            </div>
+        </div>
+        <a href="https://www.freepik.com/photos/">Technology photo created by jcomp - www.freepik.com</a>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "HomeFooter",
+  props: {
+    msg: String
+  },
+  methods:{
+    gostores(){
+      this.$router.replace('stores');
+    },
+  }
+};
+</script>
+
+<style >
+  .footer{
+    padding-top: 7rem;
+    width: 100%;
+    min-height: 500px;
+    text-align: left;
+    color: #ffffff;
+    background-color:darkslategray;
+  }
+</style>
